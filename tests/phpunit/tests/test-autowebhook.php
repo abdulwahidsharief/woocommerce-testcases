bbbde2dfa1cbc651df1c56e42e33eb354a557a65
abdulwahidsharief <abdul.sharief@razorpay.com>
2023-01-20 18:07:17 +0530

    test

---
+++ tests/phpunit/tests/test-autowebhook.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once __DIR__ . '/../mockfactory/Request.php';
+
+use Razorpay\MockApi\MockApi;
+
+class Test_AutoWebhook extends WP_UnitTestCase
+{
+    private $instance;
+
+    public function setup(): void
+    {
+        parent::setup();
+        $this->instance = Mockery::mock('WC_Razorpay')->makePartial()->shouldAllowMockingProtectedMethods();
+        $_POST = array();
+    }
+
+    public function testEmptyKeyAndSecretValidation()
+    {
+        $this->instance->shouldReceive('getSetting')->andReturnUsing(function ($key) {
+            if ($key == 'key_id')
+            {
+                return null;
+            }
+            else
+            {
+                return null;
+            }
+        });
+
+        ob_start();
+        $response = $this->instance->autoEnableWebhook();
+        $response = ob_get_contents();
+        ob_end_clean();
+
+        $this->assertStringContainsString('Key Id and Key Secret are required', $response);
+    }
+}
