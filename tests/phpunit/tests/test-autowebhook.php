07f0ab329b4541a876f15e31f3210b94bcb7628a
abdulwahidsharief <abdul.sharief@razorpay.com>
2023-01-23 01:13:16 +0530

    updated code

--- tests/phpunit/tests/test-autowebhook.php
+++ tests/phpunit/tests/test-autowebhook.php
@@ -2,8 +2,6 @@
 
 use Razorpay\MockApi\MockApi;
 
-//require_once __DIR__ . '/../../../woo-razorpay.php';
-
 class Test_AutoWebhook extends WP_UnitTestCase
 {
     private $instance;
@@ -34,5 +32,115 @@ class Test_AutoWebhook extends WP_UnitTestCase
         ob_end_clean();
 
         $this->assertStringContainsString('Key Id and Key Secret are required', $response);
+    }
+
+    public function testInvalidKeyAndSecretValidation()
+    {
+        $this->instance->shouldReceive('getSetting')->andReturnUsing(function ($key) {
+            if ($key == 'key_id')
+            {
+                return 'key_id';
+            }
+            else
+            {
+                return 'key_secret';
+            }
+        });
+
+        $this->instance->shouldReceive('getRazorpayApiInstance')->andReturnUsing(function () {
+            return new MockApi('invalid_key_id', 'invalid_key_secret');
+        });
+
+        ob_start();
+        $response = $this->instance->autoEnableWebhook();
+        $response = ob_get_contents();
+        ob_end_clean();
+
+        $this->assertStringContainsString('Please check Key Id and Key Secret', $response);
+    }
+
+    public function testWebhookFailedForLocalhost()
+    {
+        $this->instance->shouldReceive('getSetting')->andReturnUsing(function ($key) {
+            if ($key == 'key_id')
+            {
+                return 'key_id';
+            }
+            else
+            {
+                return 'key_secret';
+            }
+        });
+
+        $this->instance->shouldReceive('getRazorpayApiInstance')->andReturnUsing(function () {
+            return new MockApi('key_id', 'key_secret');
+        });
+
+        ob_start();
+        $response = $this->instance->autoEnableWebhook();
+        $response = ob_get_contents();
+        ob_end_clean();
+
+        $this->assertStringContainsString('Could not enable webhook for localhost server', $response);
+    }
+
+    public function testAutoCreateWebhook()
+    {
+        $this->instance->shouldReceive('getSetting')->andReturnUsing(function ($key) {
+            if ($key == 'key_id')
+            {
+                return 'key_id';
+            }
+            else
+            {
+                return 'key_secret';
+            }
+        });
+
+        $this->instance->shouldReceive('getRazorpayApiInstance')->andReturnUsing(function () {
+            return new MockApi('key_id', 'key_secret');
+        });
+
+        $this->instance->shouldReceive('getWebhookUrl')->andReturn("https://webhook.site/create");
+
+        $response = $this->instance->autoEnableWebhook();
+
+        $this->assertSame('create', $response['id']);
+        $this->assertSame('https://webhook.site/create', $response['url']);
+        $this->assertSame('webhook', $response['entity']);
+        $this->assertTrue($response['active']);
+        $this->assertNotNull($response['events']);
+        $this->assertTrue($response['events']['payment.authorized']);
+        $this->assertTrue($response['events']['order.paid']);
+    }
+
+    public function testAutoUpdateWebhook()
+    {
+        $this->instance->shouldReceive('getSetting')->andReturnUsing(function ($key) {
+            if ($key == 'key_id')
+            {
+                return 'key_id_1';
+            }
+            else
+            {
+                return 'key_secret';
+            }
+        });
+
+        $this->instance->shouldReceive('getRazorpayApiInstance')->andReturnUsing(function () {
+            return new MockApi('key_id_1', 'key_secret');
+        });
+
+        $this->instance->shouldReceive('getWebhookUrl')->andReturn("https://webhook.site/update");
+
+        $response = $this->instance->autoEnableWebhook();
+
+        $this->assertSame('update', $response['id']);
+        $this->assertSame('https://webhook.site/update', $response['url']);
+        $this->assertSame('webhook', $response['entity']);
+        $this->assertTrue($response['active']);
+        $this->assertNotNull($response['events']);
+        $this->assertTrue($response['events']['payment.authorized']);
+        $this->assertTrue($response['events']['order.paid']);
     }
 }
